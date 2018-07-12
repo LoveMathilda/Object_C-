71ad847ee92182dc7fd49c98485af112897190b2
雪华 <wangxuehua@wangxuehuadeMacBook-Air.local>
2018-07-12 22:14:33 +0800

    Initial Commit

---
+++ Object_C类的创建/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Object_C类的创建
+//
+//  Created by 王雪华 on 2018/7/12.
+//  Copyright © 2018年 Wangxh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
